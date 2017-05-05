ca76ec5eab0905ae22de4d0bd6e3785cc6f3663b
Hien Nguyen <hien.nguyen@terralogic.com>
2017-05-05 16:55:41 +0700

    move classes

---
+++ msis-core/src/test/java/com/msis/core/config/BeanConfig.java
@@ -0,0 +1,36 @@
+package com.msis.core.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.msis.core.service.CDNService;
+import com.msis.core.service.PatientService;
+import com.msis.core.service.UserService;
+import com.msis.core.service.impl.CDNServiceS3;
+import com.msis.core.service.impl.PatientServiceImpl;
+import com.msis.core.service.impl.UserServiceImpl;
+
+@Configuration
+public class BeanConfig {
+	static Logger log = LoggerFactory.getLogger(BeanConfig.class);
+	
+	@Bean
+	public UserService userService() {
+		log.info("Init UserService Bean");
+		return new UserServiceImpl();
+	}
+	
+	@Bean
+	public CDNService cdnService() {
+		log.info("init CDNService Bean");
+		return new CDNServiceS3();
+	}
+	
+	@Bean
+	public PatientService patientService() {
+		log.info("init PatientService Bean");
+		return new PatientServiceImpl();
+	}
+}
