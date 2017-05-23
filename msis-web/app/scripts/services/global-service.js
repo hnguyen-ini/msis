187537915bd462cefe57ef39789d9ad1fd93401d
Hien Nguyen <hien.nguyen@terralogic.com>
2017-05-23 18:03:12 +0700

    global update

--- msis-web/app/scripts/services/global-service.js
+++ msis-web/app/scripts/services/global-service.js
@@ -4,7 +4,8 @@ angular.module('webappApp')
         service.s1 = "DFGSDG233DJDPR3S";
         service.s2 = "495ae7e34b9da43b9b4bdac644529aa7";
         service.s3 = "9609f89caaa074264acf4df75122dbdc";
-        service.host = "http://localhost:8080/msis-rest/rest/";
+        //service.host = "http://localhost:8080/msis-rest/rest/"; // home
+        service.host = "http://localhost:8085/msis/rest/"; // company
         //authen
         service.registerUri = service.host + "authen/reg";
         service.signinUri = service.host + "authen/signin";
