a705e7e69873d6156505def4b64f649da1202efd
Hien Nguyen <hien.nguyen@terralogic.com>
2017-06-13 04:24:43 +0700

    patient page continue..

--- msis-web/app/scripts/controllers/patient/patients-controller.js
+++ msis-web/app/scripts/controllers/patient/patients-controller.js
@@ -2,7 +2,7 @@
  * Patient Controller
  */
 angular.module('webappApp')
-    .controller('PatientsController', ['NgTableParams', 'AuthenService', 'PatientService', '$http', function(NgTableParams, AuthenService, PatientService, $http) {
+    .controller('PatientsController', ['NgTableParams', 'AuthenService', 'PatientService', '$location', function(NgTableParams, AuthenService, PatientService, $location) {
         var vm = this;
         vm.loadData = loadData;
         vm.data = {};
@@ -17,47 +17,16 @@ angular.module('webappApp')
                 PatientService.getByCreator(cookies.currentUser.token).then(function (response) {
                     if (response.success) {
                         vm.data = response.result;
+                        vm.tableParams = new NgTableParams({count:10, sorting:{firstName:'asc', lastName:'asc'}, filter: {firstName: 'T'}}, {counts:[10,20,50,100], dataset: vm.data});
+                        vm.tableParams._settings.total = vm.data.length;
                     } else {
                         toastr.error(response.message, 'Msis-Web');
                     }
                 });
             }
 
-            PatientService.getByCreator();
         }
 
-        $http.get('data/data.json')
-            .then(function(data) {
-                vm.data = data.data.person;
-                // vm.tableParams = new NgTableParams({count:8, sorting:{name:'asc'}}, 
-                //     {
-                //         getData: function($defer, pamrams) {
-                //             vm.tableParams.data = $filter('orderBy')(vm.data.data.person, vm.tableParams._params.sorting.name);
-                //             $defer.resolve(vm.tableParams.data);
-                //         }});
-                //vm.defaultConfigTableParams = new NgTableParams({}, {dataset: vm.data})
-                vm.tableParams = new NgTableParams({count:10, sorting:{firstName:'asc', lastName:'asc'}, filter: {firstName: 'T'}}, {counts:[10,20,50,100], dataset: vm.data});
-                vm.tableParams._settings.total = vm.data.length;
-                // vm.tableParams._settings.paginationMinBlocks = 2;
-                // vm.tableParams._settings.dataset = vm.data.data.person;
-                // vm.tableParams.data = vm.data.data.person;
-
-                //vm.tableParams = createUsingFullOptions();
-                var test="";
-                // function createUsingFullOptions() {
-                //     var initialParams = {
-                //         count: 5 // initial page size
-                //     };
-                //     var initialSettings = {
-                //         // page size buttons (right set of buttons in demo)
-                //         counts: [],
-                //         // determines the pager buttons (left set of buttons in demo)
-                //         paginationMaxBlocks: 13,
-                //         paginationMinBlocks: 2,
-                //         dataset: vm.data
-                //     };
-                //     return new NgTableParams(initialParams, initialSettings);
-                // }
-            });
+        
     }
 ]);
